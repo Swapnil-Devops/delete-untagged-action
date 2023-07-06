8a1e9589f5f12d35229fe5c2110b001e2c31e025
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-07-06 12:49:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ async function main() {
   try {
     const token = core.getInput('github-token', { required: true });
     const packageName = core.getInput('package-name');
+    const namespace = core.getInput('namespace');
     const repository = core.getInput('repository');
     const github = getOctokit(token);
 
-    const accountType = 'orgs';
+    const accountType = namespace ? 'users' : 'orgs';
     const [owner, repo] = repository.split('/');
     const package = packageName || repo;
-    const getUrl = `GET /${owner}/packages/container/${package}`;
+    const getUrl = `GET /${accountType}/${owner}/packages/container/${package}`;
     const { data: metadata } = await github.request(getUrl);
     console.log("metadata=",metadata);
     core.setOutput('package-metadata', JSON.stringify(metadata));
