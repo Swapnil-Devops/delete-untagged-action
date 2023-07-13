bc91a3dfe037766eebbcf179edf22642917f36ff
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-07-13 20:23:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 const core = require('@actions/core');
 const { getOctokit } = require('@actions/github');
-const { graphql } = require('@octokit/graphql');
+// const { graphql } = require('@octokit/graphql');
 
-async function graphQuery() {
-  const token = core.getInput('github-token', { required: true });
-  const packageName = core.getInput('package-name');
-  const personalAccount = core.getInput('personal-account');
-  const repos = core.getInput('repository');
-  const github = getOctokit(token);
-  const [owner, repo] = repos.split('/');
-  const graphqlWithAuth = graphql.defaults({
-    headers: {
-      authorization: token,
-    },
-  });
-  const { repository } = await graphqlWithAuth(`
-    {
-      repository(owner: "octokit", name: "graphql.js") {
-        issues(last: 3) {
-          edges {
-            node {
-              title
-            }
-          }
-        }
-      }
-    }
-  `);
-}
+// async function graphQuery() {
+//   const token = core.getInput('github-token', { required: true });
+//   const packageName = core.getInput('package-name');
+//   const personalAccount = core.getInput('personal-account');
+//   const repos = core.getInput('repository');
+//   const github = getOctokit(token);
+//   const [owner, repo] = repos.split('/');
+//   const graphqlWithAuth = graphql.defaults({
+//     headers: {
+//       authorization: token,
+//     },
+//   });
+//   const { repository } = await graphqlWithAuth(`
+//     {
+//       repository(owner: "octokit", name: "graphql.js") {
+//         issues(last: 3) {
+//           edges {
+//             node {
+//               title
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
+// }
 async function restQuery() {
   try {
     const token = core.getInput('github-token', { required: true });
@@ -39,7 +39,7 @@ async function restQuery() {
     const accountType = personalAccount ? 'users' : 'orgs';
     const [owner, repo] = repository.split('/');
     const package = packageName || repo;
-    console.log("rest:",owner,repo, package, accountType)
+    console.log("rest:",owner,repo, package, accountType);
     const getUrl = `GET /${accountType}/${owner}/packages/container/${package}`;
     const { data: metadata } = await github.request(getUrl);
     console.log('metadata=', metadata);
