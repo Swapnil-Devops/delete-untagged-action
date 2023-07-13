74d5182f5187f100d88488106689259a79f9e10f
swapnil <swapnilgawali244@gmail.com>
2023-07-13 21:03:24 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 const core = require('@actions/core');
 const { getOctokit } = require('@actions/github');
-// const { graphql } = require('@octokit/graphql');
+const { graphql } = require('@octokit/graphql');
 
-// async function graphQuery() {
-//   const token = core.getInput('github-token', { required: true });
-//   const packageName = core.getInput('package-name');
-//   const personalAccount = core.getInput('personal-account');
-//   const repos = core.getInput('repository');
-//   const github = getOctokit(token);
-//   const [owner, repo] = repos.split('/');
-//   const graphqlWithAuth = graphql.defaults({
-//     headers: {
-//       authorization: token,
-//     },
-//   });
-//   const { repository } = await graphqlWithAuth(`
-//     {
-//       repository(owner: "octokit", name: "graphql.js") {
-//         issues(last: 3) {
-//           edges {
-//             node {
-//               title
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `);
-// }
+async function graphQuery() {
+  const token = core.getInput('github-token', { required: true });
+  const packageName = core.getInput('package-name');
+  const personalAccount = core.getInput('personal-account');
+  const repos = core.getInput('repository');
+  const github = getOctokit(token);
+  const [owner, repo] = repos.split('/');
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: token,
+    },
+  });
+  console.log("repo token",token);
+  const { repository } = await graphqlWithAuth(`
+    {
+      repository(owner: "Swapnil-Devops", name: "my-package") {
+        issues(last: 3) {
+          edges {
+            node {
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+  console.log("repo",repository);
+}
 async function restQuery() {
   try {
     const token = core.getInput('github-token', { required: true });
@@ -39,6 +41,7 @@ async function restQuery() {
     const accountType = personalAccount ? 'users' : 'orgs';
     const [owner, repo] = repository.split('/');
     const package = packageName || repo;
+    // rest: Swapnil-Devops my-package my-package orgs
     console.log("rest:",owner,repo, package, accountType);
     const getUrl = `GET /${accountType}/${owner}/packages/container/${package}`;
     const { data: metadata } = await github.request(getUrl);
@@ -50,7 +53,7 @@ async function restQuery() {
 }
 
 async function main() {
-  restQuery();
+  graphQuery();
 }
 
 main();
