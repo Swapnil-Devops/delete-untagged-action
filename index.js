2d90d2b88acb6ab28111e591ac4868411dc155e3
swapnil <swapnilgawali244@gmail.com>
2023-07-13 20:07:03 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const core = require('@actions/core');
 const { getOctokit } = require('@actions/github');
-const { graphql } = require("@octokit/graphql");
+const { graphql } = require('@octokit/graphql');
 
-async function main() {
+async function graphQuery() {
+  const token = core.getInput('github-token', { required: true });
+  const packageName = core.getInput('package-name');
+  const personalAccount = core.getInput('personal-account');
+  const repos = core.getInput('repository');
+  const github = getOctokit(token);
+  const [owner, repo] = repos.split('/');
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: token,
+    },
+  });
+  const { repository } = await graphqlWithAuth(`
+    {
+      repository(owner: "octokit", name: "graphql.js") {
+        issues(last: 3) {
+          edges {
+            node {
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+}
+async function restQuery() {
   try {
     const token = core.getInput('github-token', { required: true });
     const packageName = core.getInput('package-name');
@@ -13,13 +39,18 @@ async function main() {
     const accountType = personalAccount ? 'users' : 'orgs';
     const [owner, repo] = repository.split('/');
     const package = packageName || repo;
+    console.log("rest:",owner,repo, package, accountType)
     const getUrl = `GET /${accountType}/${owner}/packages/container/${package}`;
     const { data: metadata } = await github.request(getUrl);
-    console.log("metadata=",metadata);
+    console.log('metadata=', metadata);
     core.setOutput('package-metadata', JSON.stringify(metadata));
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function main() {
+  restQuery();
 }
 
 main();
