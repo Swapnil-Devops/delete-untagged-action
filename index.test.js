c1b369f6346d2cf757175da51b034cde237a21f5
swapnil <swapnilgawali244@gmail.com>
2023-07-18 00:07:17 +0530

    added changes in index.test.js

--- index.test.js
+++ index.test.js
@@ -1,22 +1,27 @@
-const main = require("./index");
-describe("package action tests", () => {
-  it("check if inputs are received", async () => {
-    const core = {
-      getInput: jest.fn().mockReturnValueOnce("my-value"),
-      setFailed: jest.fn(),
-      setOutput: jest.fn(),
-    };
-    const github = {
-      getOctokit: jest.fn(),
-    };
-    await main.main(core, github);
+const { main } = require('./index');
+const core = require('@actions/core');
+const github = require('@actions/github');
+// jest.mock("@actions/core");
 
-    expect(core.getInput).toHaveBeenCalledWith("github-token", {
+describe('package action tests', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('check if inputs are received', async () => {
+    const mockGetInput = jest.spyOn(core, 'getInput').mockImplementation();
+    const mockSetFailed = jest.spyOn(core, 'setFailed');
+    const mockSetOutput = jest.spyOn(core, 'setOutput').mockImplementation();
+    const mockGetOctokit = jest
+      .spyOn(github, 'getOctokit')
+      .mockImplementation();
+    main(core, github);
+
+    expect(mockGetInput).toHaveBeenCalledWith('github-token', {
       required: true,
     });
-    expect(core.getInput).toHaveBeenCalledWith("package-name");
-    expect(core.getInput).toHaveBeenCalledWith("namespace");
-    expect(core.getInput).toHaveBeenCalledWith("repository");
-    expect(github.getOctokit).toHaveBeenCalledTimes(1);
+    expect(mockGetInput).toHaveBeenCalledWith('package-name');
+    expect(mockGetInput).toHaveBeenCalledWith('namespace');
+    expect(mockGetInput).toHaveBeenCalledWith('repository');
+    expect(mockGetOctokit).toHaveBeenCalledTimes(1);
   });
 });
